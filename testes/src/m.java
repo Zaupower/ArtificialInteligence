409e226a87024ee9b8709729273884c3665190a5
Zaupower <you@example.com>
2021-05-31 13:11:21 +0100

    ai

--- testes/src/m.java
+++ testes/src/m.java
@@ -1,7 +1,7 @@
 public class m {
     public static void main(String[] args) {
         //ideia de imppleemntaçao caso
-        GA ga = new GA(10, 20, 10000, 10, 2, 0.7, 0.2);
+        GA ga = new GA(1000, 20, 400, 1, 2, 0.7, 0.2);
         ga.loopthrow();
     }
 }
